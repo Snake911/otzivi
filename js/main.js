b2787c27d63446987f16b9145c02cd23da4e730f
VolkovDV <snake1234567890-1@yandex.ru>
2021-05-30 17:50:44 +0300

    finished screen

--- js/main.js
+++ js/main.js
@@ -1,6 +1,19 @@
 $(document).ready(function() {
+    new WOW().init();
+    $('.phone').mask('+7 (999) 999-99-99');
     $('.header_burger').click(function() {
         $(this).toggleClass('opened');
         $('.mobile_contacts').toggleClass('opened');
+    });
+
+    if($(window).width() <= 1200) {
+        $('.screen_right').append($('.form_callback'));
+    }
+    $(window).resize(function() {
+        if($(window).width() <= 1200) {
+            $('.screen_right').append($('.form_callback'));
+        } else {
+            $('.screen_left').append($('.form_callback'));
+        }
     });
 });
